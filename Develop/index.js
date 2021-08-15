a6ec688f94608cb2418857226b645955e71fbf25
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-08-15 16:45:37 -0700

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+// Filler questions
+const questions = ['What is your name', 'How old are you', 'Where do you live'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
