90931c051035895b59a0fbec76a3883251cabea8
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-19 22:22:42 -0700

    using inquirer to prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,53 +1,122 @@
 // Packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require("axios");
+
+// linking to page where the README is developed
 const generate = require("./utils/generateMarkdown")
 
 // An array of questions for user input
-// Filler questions
-const questions = [
-    {type: "input",
-     name: "title",
-     message: "What is your project title?"
-    },
-    {type: "input",
-     name: "badge",
-     message: "Please provide any badges you want to include."
-    },
-    {type: "input",
-     name: "description",
-     message: "Provide a short description explaining the what, why and how of your application."
-    },
-    {type: "input",
-     name: "installation",
-     message: "Please provide any installation instructions."
-    },
-    {type: "input",
-     name: "usage",
-     message: "Provide instructions and examples for use?"
-    },
-    {type: "input",
-     name: "license",
-     message: "Please provide the license or badge link"
-    },
-    {type: "input",
-    name: "contributing",
-    message: "Please provide details if someone else would like to contribute to your project."
-   },
-    {type: "input",
-    name: "test",
-    message: "Please provide the project tests"
-   },
-   {type: "input",
-    name: "repo",
-    message: "What is your repository link?"
-   },
-  {type: "input",
-  name: "contactInfo",
-  message: "What is your GitHub username?"
- },
-];
+const questions = () => {
+    // using inquirer to prompt questions to user
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false; 
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false; 
+                }
+            }
+        }, 
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'GNU'],
+            default: ["MIT"],
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter steps required to install your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this app?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a usage description!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test', 
+            message: 'What command should be run to run tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'What does the user need to know about contributing to the repo?'
+        }
+    ]);
+};
+
 
 // A function to write the README file
 inquirer
